bb86b9c23a9099d1800c83d48c95315c092efff5
Gilberto Beltrame Neto <gilberto_abn@hotmail.com>
2020-08-13 00:04:39 -0300

    Clean

---
+++ src/Components/noteList.jsx
@@ -0,0 +1,20 @@
+import React, {Component} from 'react';
+
+class ListaDeNotas extends Component {
+    render(){
+        return (
+    <ul>
+        <li>
+          <section>
+            <header>
+              <h1>Título</h1>
+            </header>
+            <p>Escreva a sua nota</p>
+          </section>
+        </li>
+    </ul>
+        )
+    }
+}
+
+export default ListaDeNotas;
